c12236416f685aaeece2d470d37389a914e27d8a
Penko_Mlakar <dejan.penko@gmail.com>
2026-03-09 22:01:32 +0100

    Added adsense required code block to the app.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -142,14 +142,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b07933719cda0ee023e7aca016e11edc
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "67725a6a3fbc7c16de6f3ea519ee3cc2",
+"flutter_bootstrap.js": "5746007109e50db106e7858be9811411",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e5121b48c2ae8fda6fcfcd1676a40d74",
-"/": "e5121b48c2ae8fda6fcfcd1676a40d74",
-"main.dart.js": "869a3c575d0cfa2210e0c6cb6a27532f",
+"index.html": "677bac6b52e9e46a38b563e139512e49",
+"/": "677bac6b52e9e46a38b563e139512e49",
+"main.dart.js": "de4aafb91a78a51ab48753ca23a47e7c",
 "manifest.json": "06921824189cec6098c5d7016532df47",
 "version.json": "85cc9e789a03766542af2e6d7bc15235"};
 // The application shell files that are downloaded before a service worker can
