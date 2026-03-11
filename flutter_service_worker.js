7da0156516a7ab128035a272b7afbf84cc950cf2
Penko_Mlakar <dejan.penko@gmail.com>
2026-03-11 19:49:21 +0100

    Attempting to start showing ads in release.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -142,14 +142,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b07933719cda0ee023e7aca016e11edc
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "423c5eafee0d49432b599bac5269318b",
+"flutter_bootstrap.js": "dbbac313a5e85bda88e6d8ea3c2034ee",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b6edf5e2ad0631b4457fdba78dc00403",
 "/": "b6edf5e2ad0631b4457fdba78dc00403",
-"main.dart.js": "abca1a99988b7e7e7de29965d6cb9c26",
+"main.dart.js": "cc1be3bdbea3efc0e44698dac5965714",
 "manifest.json": "06921824189cec6098c5d7016532df47",
 "version.json": "85cc9e789a03766542af2e6d7bc15235"};
 // The application shell files that are downloaded before a service worker can
