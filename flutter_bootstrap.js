3838aa862e292ff0dc7d491a0f878cc4adb248d4
Penko_Mlakar <dejan.penko@gmail.com>
2026-03-11 19:52:18 +0100

    Attempting to start showing ads in release.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2997991309"
+    serviceWorkerVersion: "814962989"
   }
 });
